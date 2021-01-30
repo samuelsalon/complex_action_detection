a15e5ea5d5dcb9d5de34e57cbbed40981c172449
Samuel Saloň <salonjurajs@gmail.com>
2021-01-30 19:06:42 +0100

    Change file deepsort/generate_detections.py to tensorflow 2 version.

--- deepsort/generate_detections.py
+++ deepsort/generate_detections.py
@@ -4,7 +4,7 @@ import errno
 import argparse
 import numpy as np
 import cv2
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def _run_in_batches(f, data_dict, out, batch_size):
@@ -73,7 +73,7 @@ class ImageEncoder(object):
     def __init__(self, checkpoint_filename, input_name="images",
                  output_name="features"):
         self.session = tf.Session()
-        with tf.gfile.GFile(checkpoint_filename, "rb") as file_handle:
+        with tf.io.gfile.GFile(checkpoint_filename, "rb") as file_handle:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(file_handle.read())
         tf.import_graph_def(graph_def, name="net")
@@ -211,3 +211,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
